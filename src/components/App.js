b3b2282ca0f9cd73ceb23f4988f98021e94be2f7
jrga03 <jasonacido@gmail.com>
2018-05-16 13:11:05 +0800

    react-native-onesignal test

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,38 @@
-import React, { Component } from 'react';
-import { View } from 'react-native';
-import { RootStack } from '../router/router';
+import React, { Component } from "react";
+import { View } from "react-native";
+import { RootStack } from "../router/router";
+import OneSignal from "react-native-onesignal";
 
 export default class App extends Component {
+    componentWillMount() {
+        OneSignal.init("6ea03bc1-833c-47b4-adda-1700694659b1");
+
+        // OneSignal.addEventListener("received", this.onReceived);
+        // OneSignal.addEventListener("opened", this.onOpened);
+        OneSignal.addEventListener("ids", this.onIds);
+    }
+
+    componentWillUnmount() {
+        // OneSignal.removeEventListener("received", this.onReceived);
+        // OneSignal.removeEventListener("opened", this.onOpened);
+        OneSignal.removeEventListener("ids", this.onIds);
+    }
+
+    onReceived(notification) {
+        console.log("Notification received: ", notification);
+    }
+
+    onOpened(openResult) {
+        console.log("Message: ", openResult.notification.payload.body);
+        console.log("Data: ", openResult.notification.payload.additionalData);
+        console.log("isActive: ", openResult.notification.isAppInFocus);
+        console.log("openResult: ", openResult);
+    }
+
+    onIds(device) {
+        console.log("Device info: ", device);
+    }
+
     render() {
         return <RootStack />;
     }
